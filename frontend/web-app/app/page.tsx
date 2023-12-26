162d8873b469f8b6c4136006ef2fadc382d2e889
Elliot <elliot.j.b@hotmail.co.uk>
2023-12-26 17:30:43 +0000

    End of section 8

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Listings from './auctions/Listings';
+
+export default function Home() {
+  console.log('Server component');
+  return (
+    <div>
+      <Listings />
+    </div>
+  )
+}
