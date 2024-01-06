a4d5c6462c5b3b55a58ed5dcf93a1dd113fe08c0
Elliot <elliot.j.b@hotmail.co.uk>
2024-01-06 10:12:36 +0000

    End of section 9

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { Auction, PagedResult } from "@/types/Index";
+import { getTokenWorkAround } from "./authActions";
 
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
@@ -9,4 +10,25 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
     if (!res.ok) throw new Error('Failed to fetch data');
 
     return res.json();
+}
+
+export async function UpdateAuctionTest() {
+    const data = {
+        mileage: Math.floor(Math.random() * 100000) + 1
+    }
+
+    const token = await getTokenWorkAround();
+
+    const res = await fetch('http://localhost:6001/auctions/dc1e4071-d19d-459b-b848-b5c3cd3d151f', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token?.access_token
+        },
+        body: JSON.stringify(data)
+    })
+
+    if(!res.ok) return {status: res.status, message: res.statusText}
+
+    return res.statusText;
 }
